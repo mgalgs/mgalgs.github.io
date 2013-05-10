165ae4f2799a91fe6ce5af2c4c7b0a42072606f5
Mitchel Humpherys <mitchelh@codeaurora.org>
2013-05-10 13:17:56 -0700

    update newpost script for drafts support

--- newpost.sh
+++ newpost.sh
@@ -3,12 +3,35 @@
 MYEDITOR="emacsclient -n -a emacs"
 MYMARKUP=markdown
 
-if [[ $# -ne 1 || $1 == "-h" || $1 == "--help" ]]; then
-    echo "usage: $0 title"
-    exit 1
-fi
+usage()
+{
+    cat <<EOF
+usage: $0 [options] title
 
-newfile=_posts/$(date +%Y-%m-%d)-${1}.$MYMARKUP
+Options:
+  -d    Start a draft
+EOF
+}
+
+[[ $# -lt 1 || $1 == "--help" ]] && { usage; exit 1; }
+
+while getopts dh opt; do
+    case $opt in
+        d) do_draft=yes ;;
+        *)
+	    usage
+	    exit 1
+            ;;
+    esac
+done
+
+shift $(($OPTIND-1))
+
+if [[ "$do_draft" = "yes" ]]; then
+    newfile=_drafts/${1}.$MYMARKUP
+else
+    newfile=_posts/$(date +%Y-%m-%d)-${1}.$MYMARKUP
+fi
 
 # start with some bare bones front matter:
 cat > $newfile <<EOF
